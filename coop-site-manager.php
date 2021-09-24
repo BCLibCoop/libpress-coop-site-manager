b4ba6dc50ba812ccbd8328a77f49e76b325204e7
Sam Edwards <sam.edwards@bc.libraries.coop>
2021-09-24 10:30:38 -0700

    Fix check for contact widget

--- coop-site-manager.php
+++ coop-site-manager.php
@@ -18,7 +18,7 @@
  * @wordpress-plugin
  * Plugin Name:       Coop Site Manager
  * Description:       This is the common location for the other Coop Plugins to reside.
- * Version:           1.0.0
+ * Version:           1.1.3
  * Network:           true
  * Requires at least: 5.2
  * Requires PHP:      7.0
--- inc/json-ld.php
+++ inc/json-ld.php
@@ -73,7 +73,7 @@ class LibPressSchema
 
             // Only include address info in the contact info widget is active
             // Most federartions have this turned off
-            if (is_active_widget('coop-site-manager-widget')) {
+            if (is_active_widget(false, false, 'coop-site-manager-widget')) {
                 if (!empty($info['city']) && !empty($info['prov'])) {
                     $schema['address'] = [
                         '@type' => 'PostalAddress',
