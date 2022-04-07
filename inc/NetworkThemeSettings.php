1db361e0aec4bc163c7e9a5853f920f875916118
Sam Edwards <sam.edwards@bc.libraries.coop>
2022-04-07 13:53:14 -0700

    Show more data on libpress themes page

--- inc/NetworkThemeSettings.php
+++ inc/NetworkThemeSettings.php
@@ -4,6 +4,26 @@ namespace BCLibCoop;
 
 class NetworkThemeSettings
 {
+    private static $settings = [
+        'search' => [
+            'search_style' => 'Search Box Style',
+            'search_type' => 'Search Type',
+            'search_url' => 'Search URL',
+            'search_param' => 'Search Term Parameter',
+            'search_extra_params' => 'Extra Search Parameters',
+            'search_external' => 'External Search Site',
+        ],
+        'style' => [
+            'header_text' => 'Title and Tagline',
+            'logo_alignment' => 'Logo Alignment',
+            'logo_size' => 'Logo Size',
+            'custom_logo' => 'Logo ID',
+            'header_image' => 'Header Image',
+            'background_image' => 'Background Image',
+            'show_sidebar' => 'Show Sidebar',
+        ],
+    ];
+
     public function __construct()
     {
         add_action('network_admin_menu', [$this, 'networkAdminMenu']);
@@ -48,66 +68,83 @@ class NetworkThemeSettings
             <table class="wp-list-table widefat fixed striped table-view-list">
                 <thead>
                     <tr>
-                        <th>WP Site ID</th>
+                        <th class="column-posts">WP Site ID</th>
                         <th>Domain Name</th>
                         <th>Custom CSS</th>
                         <th>Search Settings</th>
+                        <th>Style Settings</th>
                     </tr>
                 </thead>
 
                 <?php
                 // Loop through each blog lookup options and output form
-                foreach ($blogs as $blog) {
+                foreach ($blogs as $blog) :
                     switch_to_blog($blog->blog_id);
 
                     // Custom CSS
                     $css = wp_get_custom_css();
-                    $atts = 'readonly';
 
-                    if (empty($css)) {
-                        $atts .= ' disabled';
-                    }
-
-                    // Search Settings
-                    $search_settings = [
-                        'search_style' => 'Search Box Style',
-                        'search_type' => 'Search Type',
-                        'search_url' => 'Search URL',
-                        'search_param' => 'Search Term Parameter',
-                        'search_extra_params' => 'Extra Search Parameters',
-                        'search_external' => 'External Search Site',
+                    $settings_html = [
+                        'css' => sprintf(
+                            '<textarea rows="5" style="width: 100%%;" %s>%s</textarea><span>%d lines</span>',
+                            empty($css) ? 'readonly disabled' : 'readonly',
+                            $css,
+                            substr_count($css, PHP_EOL)
+                        ),
                     ];
 
-                    $search_settings_html = '';
+                    foreach (self::$settings as $settings_section_name => $settings_section) {
+                        if (empty($settings_html[$settings_section_name])) {
+                            $settings_html[$settings_section_name] = '';
+                        }
 
-                    foreach ($search_settings as $search_setting => $search_setting_label) {
-                        $search_setting_val = get_theme_mod($search_setting);
-                        if (!empty($search_setting_val)) {
-                            $search_settings_html .= sprintf(
-                                '<div><strong>%s:</strong> %s</div>',
-                                $search_setting_label,
-                                $search_setting_val
-                            );
+                        foreach ($settings_section as $setting => $setting_label) {
+                            $setting_val = get_theme_mod($setting, null);
+
+                            if ($setting_val !== null) {
+                                // Show boolean-like as true/false
+                                if (
+                                    filter_var(
+                                        $setting_val,
+                                        FILTER_VALIDATE_BOOLEAN,
+                                        FILTER_NULL_ON_FAILURE
+                                    ) !== null
+                                ) {
+                                    $setting_val = var_export((bool) $setting_val, true);
+                                }
+
+                                // Trim image URLs down a bit
+                                $setting_val = str_replace(home_url(), '', $setting_val);
+
+                                $settings_html[$settings_section_name] .= sprintf(
+                                    '<div><strong>%s:</strong> %s</div>',
+                                    $setting_label,
+                                    $setting_val
+                                );
+                            }
                         }
                     }
 
                     // Output form
                     echo sprintf(
                         '<tr>' .
-                            '<td>%d</td><td>%s</td>' .
-                            '<td><textarea rows="5" style="width: 100%%;" %s>%s</textarea></td>' .
+                            '<td class="column-posts">%d</td>' .
+                            '<td><a href="%s">%s</a></td>' .
+                            '<td>%s</td>' .
+                            '<td>%s</td>' .
                             '<td>%s</td>' .
                         '</tr>',
                         $blog->blog_id,
+                        home_url(),
                         $blog->domain,
-                        $atts,
-                        $css,
-                        $search_settings_html
+                        $settings_html['css'],
+                        $settings_html['search'],
+                        $settings_html['style'],
                     );
 
                     // Switch back to previous blog (main network blog)
                     restore_current_blog();
-                } ?>
+                endforeach; ?>
 
             </table>
         </div><!-- .wrap -->
