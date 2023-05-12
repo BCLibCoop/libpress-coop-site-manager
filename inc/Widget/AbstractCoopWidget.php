c0f17ab7d4eea1cd51003a189d0bb729077dfd31
Sam Edwards <sam.edwards@bc.libraries.coop>
2023-05-12 15:35:13 -0700

    More to widget abstract

--- inc/Widget/AbstractCoopWidget.php
+++ inc/Widget/AbstractCoopWidget.php
@@ -2,6 +2,7 @@
 
 namespace BCLibCoop\SiteManager\Widget;
 
+use BCLibCoop\SiteManager\AbstractSiteManagerPage;
 use WP_Widget;
 
 abstract class AbstractCoopWidget extends WP_Widget
@@ -12,10 +13,30 @@ abstract class AbstractCoopWidget extends WP_Widget
     public $viewPath;
     public $options = [];
 
+    /**
+     * Potential Languages
+     *
+     * @var object[]
+     */
+    protected $languages;
+
     public function __construct()
     {
         if (empty($this->viewPath)) {
             $this->viewPath = plugin_dir_path(SITEMANAGER_PLUGIN_FILE) . "views/widget/{$this->slug}.php";
+        }
+
+        // Dummy langauges option for when Polylang is not in use
+        $this->languages = [
+            (object) [
+                'locale' => '',
+                'name' => '',
+            ],
+        ];
+
+        // Check if polylang is available and if so use its list of languages
+        if (function_exists('pll_languages_list')) {
+            $this->languages = pll_languages_list('fields');
         }
 
         parent::__construct(
@@ -27,10 +48,14 @@ abstract class AbstractCoopWidget extends WP_Widget
 
     public function form($instance)
     {
-        echo '<p class="no-options-widget">This widget is configured via the <a href="' .
-        admin_url('admin.php?page=' . $this->adminPage::$slug) . '">Site Manager</a></p>';
+        if (is_subclass_of($this->adminPage, AbstractSiteManagerPage::class)) {
+            echo '<p class="no-options-widget">This widget is configured via the <a href="' .
+            admin_url('admin.php?page=' . $this->adminPage::$slug) . '">Site Manager</a></p>';
 
-        return 'noform';
+            return 'noform';
+        } else {
+            parent::form($instance);
+        }
     }
 
     public function widget($args, $instance)
