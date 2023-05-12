5160e288bc86b7ccb3e69fc9216b559bc2fc18e8
Sam Edwards <sam.edwards@bc.libraries.coop>
2023-05-12 15:34:48 -0700

    Clean up infobanner

--- inc/InfoBanner.php
+++ inc/InfoBanner.php
@@ -9,7 +9,6 @@ class InfoBanner extends AbstractSiteManagerPage
     public static $slug = 'coop-info-banner';
     public static $page_title = 'Info Banner';
     public static $menu_title = 'Info Banner';
-    public static $shortcode = 'coop-info-banner';
 
     protected $position = 5;
 
