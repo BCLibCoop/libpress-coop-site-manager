3c44f23e07ebab3e13831a98d3f35fed562e28f3
Sam Edwards <sam.edwards@bc.libraries.coop>
2023-05-16 13:53:13 -0700

    Add note about year placeholder

--- views/admin/coop-footer.php
+++ views/admin/coop-footer.php
@@ -1,3 +1,5 @@
+<p class="description">You may use <code>{{year}}</code> to show the current year.</p>
+
 <table class="form-table">
     <?php foreach ($this->languages as $curlang) : ?>
         <?php
