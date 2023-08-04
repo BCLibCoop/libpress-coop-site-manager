bb429a93b89647353c9ef82344371fcc9dedc58c
Sam Edwards <sam.edwards@bc.libraries.coop>
2023-08-04 11:09:24 -0700

    Add title to google maps iframe

--- views/widget/coop-location-map-widget.php
+++ views/widget/coop-location-map-widget.php
@@ -10,7 +10,8 @@ extract(CoopLocationMap::getMapData());
 
 <?php if (!empty($full_address)) : ?>
     <?php if ($data['width'] > 0 && $data['height'] > 0) : ?>
-        <iframe width="<?= esc_attr($data['width']) ?>" height="<?= esc_attr($data['height']) ?>" style="border:0" src="<?= esc_attr($gmaps_url) ?>" allowfullscreen>
+        <iframe width="<?= esc_attr($data['width']) ?>" height="<?= esc_attr($data['height']) ?>" style="border:0"
+            title="Google Maps" src="<?= esc_attr($gmaps_url) ?>" allowfullscreen>
         </iframe>
     <?php else : ?>
         <!-- Map iframe set to 0x0, not displaying -->
