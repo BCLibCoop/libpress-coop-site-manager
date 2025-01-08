ba0acb09e354838c652011add03131ba9c5049ab
Sam Edwards <sam.edwards@bc.libraries.coop>
2025-01-08 13:41:19 -0800

    Update hours widget markup

--- views/widget/hours-widget.php
+++ views/widget/hours-widget.php
@@ -17,39 +17,42 @@ $notes = stripslashes(get_option('coop-hours-notes'));
 <?= $after_title ?>
 
 <?php if (!empty($days)) : ?>
-    <ul class="operating-hours">
+    <dl class="operating-hours">
 
         <?php foreach ($days as $key => $value) : ?>
-            <li class="hours-day <?= esc_attr($key) ?>">
-                <span class="hours-dow"><?php _e(ucfirst($key), 'pll_string'); ?></span>
+            <div class="row g-0 hours-day <?= esc_attr($key) ?>">
+                <dt class="col">
+                    <span class="hours-dow"><?php _e(ucfirst($key), 'pll_string'); ?></span>
+                </dt>
+                <dd class="col">
+                    <?php if ($value['notopen']) : ?>
+                        <span class="hours-notopen"><?php _e('Closed', 'pll_string'); ?></span>
+                    <?php else : ?>
+                        <?php foreach ([1, 2] as $period) : ?>
+                            <?php $period_array = $period > 1 ? "_$period" : ''; ?>
 
-                <?php if ($value['notopen']) : ?>
-                    <span class="hours-notopen"><?php _e('Closed', 'pll_string'); ?></span>
-                <?php else : ?>
-                    <?php foreach ([1, 2] as $period) : ?>
-                        <?php $period_array = $period > 1 ? "_$period" : ''; ?>
-
-                        <?php if (!empty($value['open' . $period_array])) : ?>
-                            <?php if ($period > 1) : ?>
-                                <span class="period-separator">&amp;</span>
-                            <?php endif; ?>
-
-                            <span class="period-<?= esc_attr($period) ?>">
-                                <span class="hours-open"><?= $value['open' . $period_array] ?></span>
-
-                                <?php if (!empty($value['close' . $period_array])) : ?>
-                                    <span class="hours-separator">&ndash;</span>
-                                    <span class="hours-close"><?= $value['close' . $period_array] ?></span>
+                            <?php if (!empty($value['open' . $period_array])) : ?>
+                                <?php if ($period > 1) : ?>
+                                    <span class="period-separator">&amp;</span>
                                 <?php endif; ?>
-                            </span>
-                        <?php endif; ?>
-                    <?php endforeach; ?>
-                <?php endif; ?>
 
-            </li>
+                                <span class="period-<?= esc_attr($period) ?>">
+                                    <span class="hours-open"><?= $value['open' . $period_array] ?></span>
+
+                                    <?php if (!empty($value['close' . $period_array])) : ?>
+                                        <span class="hours-separator">&ndash;</span>
+                                        <span class="hours-close"><?= $value['close' . $period_array] ?></span>
+                                    <?php endif; ?>
+                                </span>
+                            <?php endif; ?>
+                        <?php endforeach; ?>
+                    <?php endif; ?>
+                </dd>
+            </div>
+
         <?php endforeach; ?>
 
-    </ul><!-- .operating-hours -->
+    </dl><!-- .operating-hours -->
 <?php endif; ?>
 
 <?php if (!empty($notes)) : ?>
