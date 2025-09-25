0fdf1cf39c008fc395936940aeb26400ed12028a
Sam Edwards <sam.edwards@bc.libraries.coop>
2025-09-25 12:16:50 -0700

    v3.9.1

--- coop-site-manager.php
+++ coop-site-manager.php
@@ -18,7 +18,7 @@
  * @wordpress-plugin
  * Plugin Name:       Coop Site Manager
  * Description:       LibPress-specific Site Manager functionality
- * Version:           3.9.0
+ * Version:           3.9.1
  * Network:           true
  * Requires at least: 5.9
  * Requires PHP:      7.4
